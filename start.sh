16693b8aab1c25abd26dd178ac1c1b2867599a4a
Ivan-Knyazev <Ivan.Knyazev.2005@yandex.ru>
2022-10-02 01:46:42 +0400

    update start files

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Customization virtual environment..."
+sudo apt install -y python3-venv
+python3 -m venv venv
+source venv/bin/activate
+sudo pip3 install -r requirements.txt
+# echo "+ Virtual environment was activated!"
+
+echo "Create DataBase..."
+python3 init_db.py
+# echo "DataBase was created! ..."
+
+echo "Application start..."
+python3 main.py
+echo "-------------------------"
+echo "Follow the 127.0.0.1:8000"
